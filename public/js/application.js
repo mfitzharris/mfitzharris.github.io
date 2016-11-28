50fd4020f363b507d4f8bcca44c4e9db44c3c4a0
mfitzharris <molly.fitzharris@gmail.com>
2016-11-28 16:01:13 -0600

    Use ajax to dynamically load sections of site

--- public/js/application.js
+++ public/js/application.js
@@ -1,7 +1,20 @@
 $(document).ready(function() {
-  // This is called after the document has loaded in its entirety
-  // This guarantees that any elements we bind to will exist on the page
-  // when we try to bind to them
 
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+  if ($("#floating").length === 1){
+   $("#navigation").hide(); 
+  }
+  
+  $("body").on("click", ".int-link", function(event){
+    event.preventDefault();
+    $("#navigation").show();
+    $("#floating").remove();
+    var clickedUrl = $(this).attr("href")
+    $.ajax({
+      url: clickedUrl
+    })
+    .done(function(response){
+      $("#content-wrapper").html(response);
+    })
+  });
+
 });
